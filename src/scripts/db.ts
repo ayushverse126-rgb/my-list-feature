1273a83c141760ed51e5c9b786c999712e3b5e7a
isoayush <ayushverse126@gmail.com>
2025-11-18 19:12:41 +0530

    refactor: removed unnecessary logs

--- src/scripts/db.ts
+++ src/scripts/db.ts
@@ -5,7 +5,6 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export async function connectDB() {
-  console.log(process.env.MONGODB_URI)
   const uri = process.env.MONGODB_URI || "mongodb://localhost:27017";
   await mongoose.connect(uri);
   console.log("Connected to MongoDB");
